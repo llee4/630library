e536e954c6c0955cae15f860392407e0ab4ddd88
elanor <elanor.m.moore@gmail.com>
2020-11-05 18:04:52 -0500

    Add documentation, make minor changes to return of ArrayLists, and add import and package statements
    In Players.java, line 22 had a warning because the return for the Deck.getDeck() is an ArrayList of unspecified type, being converted to an ArrayList of Integers in line 22 of Players. Now, since all current ArrayLists are of Integers their getter methods return ArrayLists of Integers

--- src/Players.java
+++ src/Players.java
@@ -1,25 +1,49 @@
-class Players{
-	
-	private ArrayList<Integer> handcards = new ArrayList <Integer>();
-	private ArrayList<Integer> woncards = new ArrayList <Integer>();
+package com.github.llee4;
+
+import java.util.ArrayList;
+
+/**
+ * Represents the game players
+ */
+class Players {
+
+	/**
+	 * Holds the cards in the players' 'hand'
+	 */
+	private ArrayList<Integer> handcards = new ArrayList<Integer>();
+	/**
+	 * Holds the won cards
+	 */
+	private ArrayList<Integer> woncards = new ArrayList<Integer>();
+	/**
+	 * Represents the deck being used
+	 */
 	private Deck newDeck = new Deck();
 	private ArrayList<Integer> currentDeck = newDeck.getDeck();
-	
-	public Players () {
-	for(int i = 0; i<10; i++) {
-	handcards.add(currentDeck.get(0));	
-	currentDeck.remove(0);
-	}//close for-loop	
 
-	}//close constructor
-	
-	public ArrayList getHandcards( ){
+	/**
+	 * Creates a Players object
+	 */
+	public Players() {
+		for (int i = 0; i < 10; i++) {
+			handcards.add(currentDeck.get(0));
+			currentDeck.remove(0);
+		} // close for-loop
+
+	}// close constructor
+
+	/**
+	 * Returns the cards in hand
+	 */
+	public ArrayList<Integer> getHandcards() {
 		return handcards;
 	}
-	public ArrayList getWoncards( ){
+
+	/**
+	 * Returns the won cards
+	 */
+	public ArrayList<Integer> getWoncards() {
 		return woncards;
 	}
 
-	
-	
-}//close Players Class
+}// close Players Class
